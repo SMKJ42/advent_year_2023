953cabffd44678d307f54a1dc67444ac8d0d7d41
SKMJ42 <williamrdeason1@gmail.com>
2024-05-12 19:25:49 -0400

    solved day 2

--- src/output/day_1.rs
+++ src/output/day_1.rs
@@ -2,40 +2,114 @@ use crate::input::read_data;
 
 pub fn problem_1() -> String {
     let data = read_data(1);
-    if data.is_some() {
-        let mut total = 0;
-        let data = data.unwrap();
-        for line in data.lines() {
-            let mut num_s = String::new();
-            // grab out all valid args
-            for char in line.chars() {
-                if char.is_digit(10) {
-                    num_s.push(char);
-                }
+    let mut total = 0;
+    let data = data.unwrap();
+    for line in data.lines() {
+        let mut num_s = String::new();
+        // grab out all valid args
+        for char in line.chars() {
+            if char.is_digit(10) {
+                num_s.push(char);
             }
-
-            // get the first and last char
-            let mut chars = num_s.chars();
-            let first_char = chars.next();
-            let mut last_char = chars.last();
-            if first_char.is_none() {
-                continue;
-            }
-            if last_char.is_none() {
-                last_char = first_char;
-            }
-
-            // combine the first and last char, then add to the total
-            let mut string = String::from(first_char.unwrap());
-            string.push(last_char.unwrap());
-            total += string.parse::<i32>().unwrap();
         }
-        return total.to_string();
-    }
 
-    return "Day 1, Problem 1 not solved yet".to_string();
+        // get the first and last char
+        let mut chars = num_s.chars();
+        let first_char = chars.next();
+        let mut last_char = chars.last();
+        if first_char.is_none() {
+            continue;
+        }
+        if last_char.is_none() {
+            last_char = first_char;
+        }
+
+        // combine the first and last char, then add to the total
+        let mut string = String::from(first_char.unwrap());
+        string.push(last_char.unwrap());
+        total += string.parse::<i32>().unwrap();
+    }
+    return total.to_string();
 }
 
 pub fn problem_2() -> String {
-    return "Day 1, Problem 2 not solved yet".to_string();
+    let data = read_data(1);
+    let mut total = 0;
+    let data = data.unwrap();
+
+    for line in data.lines() {
+        let mut num_s = String::new();
+        let chars = line.chars();
+
+        for (i, char) in chars.clone().enumerate() {
+            if char.is_digit(10) {
+                num_s.push(char);
+            } else {
+                let substr = chars.clone().into_iter().skip(i).collect::<String>();
+                let val = starts_with_num_word(&substr);
+                if val != 0 {
+                    println!("val: {}", val);
+                    num_s.push_str(val.to_string().as_str());
+                }
+            }
+        }
+
+        // get the first and last char
+        let mut chars = num_s.chars();
+        let first_char = chars.next();
+        let mut last_char = chars.last();
+        if first_char.is_none() {
+            continue;
+        }
+        if last_char.is_none() {
+            last_char = first_char;
+        }
+
+        // combine the first and last char, then add to the total
+        let mut string = String::from(first_char.unwrap());
+        string.push(last_char.unwrap());
+        total += string.parse::<i32>().unwrap();
+    }
+    return total.to_string();
+}
+
+fn starts_with_num_word(string: &str) -> i32 {
+    let opts = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    for opt in opts.iter() {
+        if string.starts_with(opt) {
+            match *opt {
+                "one" => {
+                    return 1;
+                }
+                "two" => {
+                    return 2;
+                }
+                "three" => {
+                    return 3;
+                }
+                "four" => {
+                    return 4;
+                }
+                "five" => {
+                    return 5;
+                }
+                "six" => {
+                    return 6;
+                }
+                "seven" => {
+                    return 7;
+                }
+                "eight" => {
+                    return 8;
+                }
+                "nine" => {
+                    return 9;
+                }
+                _ => {}
+            }
+        }
+    }
+    return 0;
 }
--- src/output/day_2.rs
+++ src/output/day_2.rs
@@ -1,7 +1,143 @@
+use crate::input::read_data;
+
 pub fn problem_1() -> String {
-    return "Day 2, Problem 1 not solved yet".to_string();
+    let data = read_data(2);
+    let bag = Bag::new(12, 13, 14);
+    let mut sum_id = 0;
+
+    for line in data.unwrap().lines() {
+        let line = line.split_once(":").unwrap();
+        let game_id: i32 = str::parse::<i32>(line.0.split(" ").nth(1).unwrap()).unwrap();
+        let round = line.1.split(";");
+        let mut valid = true;
+        for results in round {
+            let results = results.split(",");
+            let mut result = Result::new();
+            for color_input in results {
+                let color_input = color_input.trim().split_once(" ").unwrap();
+                let count = str::parse::<i32>(color_input.0).unwrap();
+                let color = Color::from_str(color_input.1);
+                match color {
+                    Color::Red => result.set_red(count),
+                    Color::Green => result.set_green(count),
+                    Color::Blue => result.set_blue(count),
+                }
+            }
+            if !result.is_valid(&bag) {
+                valid = false;
+                break;
+            }
+        }
+        if valid {
+            println!("Game ID: {}", game_id);
+            sum_id += game_id;
+        }
+    }
+
+    return sum_id.to_string();
 }
 
 pub fn problem_2() -> String {
-    return "Day 2, Problem 2 not solved yet".to_string();
+    let data = read_data(2);
+    let mut pow_sum = 0;
+
+    for line in data.unwrap().lines() {
+        let line = line.split_once(":").unwrap();
+        let round = line.1.split(";");
+        let mut result = Result::new();
+        for results in round {
+            let results = results.split(",");
+            for color_input in results {
+                let color_input = color_input.trim().split_once(" ").unwrap();
+                let count = str::parse::<i32>(color_input.0).unwrap();
+                let color = Color::from_str(color_input.1);
+                match color {
+                    // Find way to eliminate the redundancy
+                    Color::Red => {
+                        if count > result.red {
+                            result.set_red(count)
+                        }
+                    }
+                    Color::Green => {
+                        if count > result.green {
+                            result.set_green(count)
+                        }
+                    }
+                    Color::Blue => {
+                        if count > result.blue {
+                            result.set_blue(count)
+                        }
+                    }
+                }
+            }
+        }
+        println!("{:?}", result);
+        pow_sum += result.red * result.green * result.blue;
+    }
+    return pow_sum.to_string();
+}
+
+#[derive(Debug)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+impl Color {
+    fn from_str(s: &str) -> Color {
+        match s {
+            "red" => Color::Red,
+            "green" => Color::Green,
+            "blue" => Color::Blue,
+            _ => panic!("Invalid color"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Bag {
+    red: i32,
+    green: i32,
+    blue: i32,
+}
+
+impl Bag {
+    fn new(red: i32, green: i32, blue: i32) -> Bag {
+        Bag { red, green, blue }
+    }
+}
+
+#[derive(Debug)]
+
+struct Result {
+    red: i32,
+    green: i32,
+    blue: i32,
+}
+
+impl Result {
+    fn new() -> Result {
+        Result {
+            red: 0,
+            green: 0,
+            blue: 0,
+        }
+    }
+
+    fn set_red(&mut self, red: i32) {
+        self.red = red;
+    }
+
+    fn set_green(&mut self, green: i32) {
+        self.green = green;
+    }
+
+    fn set_blue(&mut self, blue: i32) {
+        self.blue = blue;
+    }
+
+    fn is_valid(&self, bag: &Bag) -> bool {
+        self.red <= bag.red && self.green <= bag.green && self.blue <= bag.blue
+    }
 }
